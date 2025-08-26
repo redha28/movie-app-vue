3d408e33020faae242f2f769686e55b125571229
redha28 <redhadefinto28@gmail.com>
2025-08-26 16:04:08 +0700

    [add]: create components for CategoryItem, FreeToWatchItem, NowPlayingItem, SkeletonLoading, and TrendingAllItem to display movie and TV show details

---
+++ src/components/Home/SkeletonLoading.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- card -->
+  <div class="bg-white rounded shadow-xl pb-3">
+    <!-- image -->
+    <div class="h-52 bg-gray-200 rounded-tr rounded-tl animate-pulse"></div>
+
+    <div class="p-2">
+      <div class="h-6 w-2/4 rounded-sm bg-gray-200 animate-pulse mb-2"></div>
+      <div class="h-4 w-4/5 rounded-sm bg-gray-200 animate-pulse mb-4"></div>
+      <div class="flex justify-between">
+        <div class="h-4 w-2/4 rounded-sm bg-gray-200 animate-pulse"></div>
+        <div class="h-4 w-1/6 rounded-sm bg-gray-200 animate-pulse"></div>
+      </div>
+    </div>
+  </div>
+</template>
