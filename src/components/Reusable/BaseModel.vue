63fbefe9f1dfbf5c4e6fee81523d2bbcbb25b77e
redha28 <redhadefinto28@gmail.com>
2025-08-26 16:04:30 +0700

    [add]: create Accordion, BaseModel, and Button components for reusable UI elements

---
+++ src/components/Reusable/BaseModel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    @click.self="movieStore.changeModalStatus()"
+    class="fixed w-full flex justify-center px-8 z-40"
+    :class="backDropModal"
+  >
+    <div v-if="movieStore.modalStatus" class="p-4 bg-white self-start mt-32 w-full sm:w-2/4">
+      <slot />
+      <button
+        class="bg-red-500 mt-4 rounded-sm py-2 px-4 text-white"
+        @click="movieStore.changeModalStatus()"
+      >
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useMoviesStore } from '@/stores/movies.js'
+import { computed } from 'vue'
+
+const movieStore = useMoviesStore()
+const backDropModal = computed(() => {
+  return movieStore.modalStatus == true ? 'bg-black bg-opacity-30 inset-0' : ''
+})
+</script>
---
+++ src/components/Reusable/Button.vue
@@ -0,0 +1,41 @@
+<template>
+  <button :class="buttonClass" @click="$emit('event')">
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String
+  },
+  rounded: {
+    type: String
+  }
+})
+const buttonClass = computed(() => {
+  return {
+    [props.type]: true,
+    [props.rounded]: true
+  }
+})
+</script>
+
+<style scoped>
+button {
+  @apply px-3 min-w-max py-1 text-xs font-poppins;
+}
+.primary {
+  @apply bg-secondary text-white;
+}
+
+.secondary {
+  @apply bg-gray-100;
+}
+</style>
